c89e56c4f939b23564a8beabdb4dd29af8c3a67c
blambo10 <blamb@live.com.au>
2025-08-02 19:24:14 +1000

    added docstrings to helper functions

--- contracts/helpers/GeoHelpers.sol
+++ contracts/helpers/GeoHelpers.sol
@@ -2,6 +2,11 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 library GeoUtils {
+    /**
+     * @notice Function to verify long / lat are within valid ranges.
+     * @param _x Longitude location of sensor.
+     * @param _y Latitude location of sensor.
+     */
     function validateCoordinates(int _x, int _y) internal pure returns (bool) {
         bool isValid = true;
         
--- contracts/helpers/StringHelpers.sol
+++ contracts/helpers/StringHelpers.sol
@@ -2,6 +2,11 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 library StringUtils {
+    /**
+    * @notice Function to verify string length and sanitize for spaces.
+    * @param _maxLength maximum string length.
+    * @param _string string to verify.
+    */
     function verifyCharacterLimit(uint _maxLength, string memory _string) internal pure returns (bool) {
         bytes1 asciiSpaceHex = hex"20";
         bytes memory stringInBytes = bytes(_string);
